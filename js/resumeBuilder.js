6928fcc77dadede3699eb9a422635056ab4fac83
Andi <andicbuchanan@gmail.com>
2016-01-08 13:20:32 +1300

    add name title and skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Andi Buchanan"
+var role = "Web Developer"
+var skills = ["html", "css", "JavaScript"]
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#main").append(skills);
